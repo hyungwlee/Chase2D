fd1dd9dacb49344bdbb5053142878559e93b5cab
Roshan Thapa Magar <roshanthapamagar@Roshans-iMac.truman.edu>
2024-10-25 15:28:16 -0500

    new project started

---
+++ Chase2D/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Chase2D
+//
+//  Created by Roshan Thapa Magar on 10/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Chase2DUITests/Chase2DUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Chase2DUITestsLaunchTests.swift
+//  Chase2DUITests
+//
+//  Created by Roshan Thapa Magar on 10/25/24.
+//
+
+import XCTest
+
+final class Chase2DUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
